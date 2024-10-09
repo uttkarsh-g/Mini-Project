2e148db33bb4f8ce6f40412e2e895e1546199d52
Alexhawak <hloji999@gmail.com>
2024-10-09 22:22:21 +0530

    first mini project

---
+++ Project-Code/01.Landing-page/js.js
@@ -0,0 +1,23 @@
+const ham = () => {
+  const menuIcon = document.querySelector(".o");
+  const closeIcon = document.querySelector(".c");
+  const navLinks = document.querySelector(".navlink");
+  const navToggle = document.querySelector(".toggle");
+
+  menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    navToggle.classList.toggle("navtoggle");
+    menuIcon.style.display = "none";
+    navLinks.style.display = "flex";
+    closeIcon.style.display = "block";
+  });
+
+  closeIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    navToggle.classList.toggle("navtoggle");
+    closeIcon.style.display = "none";
+    navLinks.style.display = "none";
+    menuIcon.style.display = "block";
+  });
+};
+ham();
