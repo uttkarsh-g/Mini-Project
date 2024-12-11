ae33c2437c0396ec8462c8420052415ed131d8eb
Alexhawak <hloji999@gmail.com>
2024-12-11 18:30:26 +0530

    fixed css and js for mobile devices

--- Project-Code/07.Mouse-follower/app.js
+++ Project-Code/07.Mouse-follower/app.js
@@ -1,23 +1,26 @@
 const m = document.querySelector("#mf");
 const b = document.body;
-b.addEventListener("mouseenter", () => {
-  m.style.opacity = 1;
-});
-b.addEventListener("mousemove", (e) => {
-  m.style.left = e.x - 18 + "px";
-  m.style.top = e.y - 14 + "px";
-});
-b.addEventListener("mouseleave", () => {
-  m.style.opacity = "";
-});
 
-b.addEventListener("touchstart", () => {
+const move = (x, y) => {
+  m.style.left = x - 18 + "px";
+  m.style.top = y - 14 + "px";
+};
+
+const hide = () => {
+  m.style.opacity = "";
+};
+const show = () => {
   m.style.opacity = 1;
+};
+
+b.addEventListener("mousemove", (e) => {
+  move(e.x, e.y);
 });
+b.addEventListener("mouseenter", show);
+b.addEventListener("mouseleave", hide);
+b.addEventListener("touchstart", show);
 b.addEventListener("touchmove", (e) => {
-  m.style.left = e.x - 18 + "px";
-  m.style.top = e.y - 14 + "px";
+  const touch = e.touches[0];
+  move(touch.clientX, touch.clientY);
 });
-b.addEventListener("touchend", () => {
-  m.style.opacity = 0;
-});
+b.addEventListener("touchend", hide);
